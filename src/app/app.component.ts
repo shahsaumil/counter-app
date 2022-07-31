685fc0f094e76356c95f98e6b42c0289a245c437
shahsaumil <shah3o@uwindsor.ca>
2022-07-31 10:55:04 -0400

    Counetr-App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title: string = 'Counter App';
+  count: number = 0;
+
+  handleIncrease = () => {
+    if(this.count == 10){
+      this.count = this.count + 15;
+    }
+    this.count = this.count + 1;
+  };
+
+  handleDecraese = () => {
+    this.count = this.count - 1;
+  };
+
+  handleReset = () => {
+    this.count = 0;
+  };
 }
